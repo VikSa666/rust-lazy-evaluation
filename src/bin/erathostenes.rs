494fe753b6a635d34341cf049f3a2abc661e74d2
Víctor Santiago <victorsantiago.matesub@gmail.com>
2022-01-26 10:26:48 +0100

    First Commit

---
+++ src/bin/erathostenes.rs
@@ -0,0 +1,16 @@
+
+
+///
+/// Function that implements the Sieve of Eratosthenes.
+/// 
+/// Given a number n, we get a list of all the prime numbers less than n
+/// 
+fn erathostenes(n: isize) -> Vec<isize> {
+    todo!()
+}
+
+fn main() {
+    let n = 50;
+    let v = erathostenes(n);
+    todo!()
+}
